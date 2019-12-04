238e9d366e3e9c78772dafd7bff79ec9793fbb72
Oleksii Valuiskyi <alex.valuiskyi@gmail.com>
2019-12-04 23:32:49 +0200

    Added endpoint routing

---
+++ src/prometheus-net.Contrib/Endpoints/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Prometheus;
+using Prometheus.Contrib.Endpoints;
+
+namespace Microsoft.AspNetCore.Routing
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static IEndpointConventionBuilder MapMetrics(this IEndpointRouteBuilder endpoints, string pattern = "metrics", CollectorRegistry registry = null)
+        {
+            var pipeline = endpoints.CreateApplicationBuilder()
+                .UseMiddleware<MetricEndpointMiddleware>(new MetricEndpointMiddleware.Settings
+                {
+                    Registry = registry
+                })
+                .Build();
+
+            return endpoints.Map(pattern, pipeline).WithDisplayName("Prometheus Metrics");
+        }
+    }
+}
