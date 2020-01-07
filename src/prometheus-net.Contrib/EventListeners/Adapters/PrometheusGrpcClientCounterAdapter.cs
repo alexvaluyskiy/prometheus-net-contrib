791fa3b7ee41ce3a97c9d3ecf5ce60807879cb61
Alex Valuyskiy <alexvaluyskiy@users.noreply.github.com>
2020-01-07 21:35:51 +0200

    Update PrometheusGrpcClientCounterAdapter.cs

--- src/prometheus-net.Contrib/EventListeners/Adapters/PrometheusGrpcClientCounterAdapter.cs
+++ src/prometheus-net.Contrib/EventListeners/Adapters/PrometheusGrpcClientCounterAdapter.cs
@@ -16,12 +16,12 @@ namespace Prometheus.Contrib.EventListeners.Adapters
 
         private static class GrpcClientPrometheusCounters
         {
-            public static Gauge GrpcClientTotalCalls = Metrics.CreateGauge("grpc_client_counters_total_calls", "Total Calls");
-            public static Gauge GrpcClientCurrentCalls = Metrics.CreateGauge("grpc_client_counters_current_calls", "Current Calls");
-            public static Gauge GrpcClientCallsFailed = Metrics.CreateGauge("grpc_client_counters_calls_failed", "Total Calls Failed");
-            public static Gauge GrpcClientCallsDeadlineExceeded = Metrics.CreateGauge("grpc_client_counters_calls_deadline_exceeded", "Total Calls Deadline Exceeded");
-            public static Gauge GrpcClientMessagesSent = Metrics.CreateGauge("grpc_client_counters_messages_sent", "Total Messages Sent");
-            public static Gauge GrpcClientMessagesReceived = Metrics.CreateGauge("grpc_client_counters_messages_received", "Total Messages Received");
+            public static Gauge GrpcClientTotalCalls = Metrics.CreateGauge("grpc_client_calls_total", "Total Calls");
+            public static Gauge GrpcClientCurrentCalls = Metrics.CreateGauge("grpc_client_calls_current_total", "Current Calls");
+            public static Gauge GrpcClientCallsFailed = Metrics.CreateGauge("grpc_client_calls_failed_total", "Total Calls Failed");
+            public static Gauge GrpcClientCallsDeadlineExceeded = Metrics.CreateGauge("grpc_client_calls_deadline_exceeded_total", "Total Calls Deadline Exceeded");
+            public static Gauge GrpcClientMessagesSent = Metrics.CreateGauge("grpc_client_messages_sent_total", "Total Messages Sent");
+            public static Gauge GrpcClientMessagesReceived = Metrics.CreateGauge("grpc_client_messages_received_total", "Total Messages Received");
         }
 
         public void OnCounterEvent(string name, double value)
