4ae760e28ff5fce7d1ffb6c71afc3bc18c2f1fa3
Fausto David Suarez Rosario <faustodavid@hotmail.com>
2021-04-13 21:52:29 +0200

    make PrometheusCounters non-static, add test for happy-flow

--- prometheus-net.Contrib.Tests/HttpClientListenerHandlerTests.cs
+++ prometheus-net.Contrib.Tests/HttpClientListenerHandlerTests.cs
@@ -23,7 +23,8 @@ namespace prometheus_net.Contrib.Tests
             apiStub.Start();
 
             var services = new ServiceCollection();
-            services.AddPrometheusHttpClientMetrics();
+            var counters = new HttpClientListenerHandler.PrometheusCounters();
+            services.AddPrometheusHttpClientMetrics(counters);
             services.AddHttpClient("test").ConfigureHttpClient(options =>
             {
                 options.BaseAddress = new Uri(apiStub.Address);
@@ -38,8 +39,33 @@ namespace prometheus_net.Contrib.Tests
                 await client.GetAsync("/", cancellationTokenSource.Token));
 
             Assert.Equal(1,
-                HttpClientListenerHandler.PrometheusCounters.HttpClientRequestsDuration
+                counters.HttpClientRequestsDuration
                     .WithLabels("0", client.BaseAddress.Host).Count);
+        }
+        
+        [Fact]
+        public async Task HttpClient_requests_should_increase_metric()
+        {
+            var apiStub = new ApiStub();
+            apiStub.Get("/", (request, args) => "ok");
+            apiStub.Start();
+
+            var services = new ServiceCollection();
+            var counters = new HttpClientListenerHandler.PrometheusCounters();
+            services.AddPrometheusHttpClientMetrics(counters);
+            services.AddHttpClient("test").ConfigureHttpClient(options =>
+            {
+                options.BaseAddress = new Uri(apiStub.Address);
+            });
+
+            var provider = services.BuildServiceProvider();
+            var client = provider.GetService<IHttpClientFactory>().CreateClient("test");
+
+            await client.GetAsync("/");
+
+            Assert.Equal(1,
+                counters.HttpClientRequestsDuration
+                    .WithLabels("200", client.BaseAddress.Host).Count);
         }
     }
 }
--- src/prometheus-net.Contrib/Diagnostics/HttpClientListenerHandler.cs
+++ src/prometheus-net.Contrib/Diagnostics/HttpClientListenerHandler.cs
@@ -6,9 +6,11 @@ namespace Prometheus.Contrib.Diagnostics
 {
     public class HttpClientListenerHandler : DiagnosticListenerHandler
     {
-        public static class PrometheusCounters
+        private readonly PrometheusCounters counters;
+
+        public class PrometheusCounters
         {
-            public static readonly Histogram HttpClientRequestsDuration = Metrics.CreateHistogram(
+            public readonly Histogram HttpClientRequestsDuration = Metrics.CreateHistogram(
                 "http_client_requests_duration_seconds",
                 "Time between first byte of request headers sent to last byte of response received.",
                 new HistogramConfiguration
@@ -17,7 +19,7 @@ namespace Prometheus.Contrib.Diagnostics
                     Buckets = Histogram.ExponentialBuckets(0.001, 2, 16)
                 });
 
-            public static readonly Counter HttpClientRequestsErrors = Metrics.CreateCounter(
+            public readonly Counter HttpClientRequestsErrors = Metrics.CreateCounter(
                 "http_client_requests_errors_total",
                 "Total HTTP requests sent errors.");
         }
@@ -25,8 +27,9 @@ namespace Prometheus.Contrib.Diagnostics
         private readonly PropertyFetcher<object> stopResponseFetcher = new PropertyFetcher<object>("Response");
         private readonly PropertyFetcher<object> stopRequestFetcher = new PropertyFetcher<object>("Request");
 
-        public HttpClientListenerHandler(string sourceName) : base(sourceName)
+        public HttpClientListenerHandler(string sourceName, PrometheusCounters counters) : base(sourceName)
         {
+            this.counters = counters;
         }
 
         public override void OnStopActivity(Activity activity, object payload)
@@ -35,7 +38,7 @@ namespace Prometheus.Contrib.Diagnostics
 
             if (response is HttpResponseMessage httpResponse)
             {
-                PrometheusCounters.HttpClientRequestsDuration
+                counters.HttpClientRequestsDuration
                     .WithLabels(httpResponse.StatusCode.ToString("D"), httpResponse.RequestMessage.RequestUri.Host)
                     .Observe(activity.Duration.TotalSeconds);
                 return;
@@ -45,7 +48,7 @@ namespace Prometheus.Contrib.Diagnostics
 
             if (request is HttpRequestMessage httpRequest)
             {
-                PrometheusCounters.HttpClientRequestsDuration
+                counters.HttpClientRequestsDuration
                     .WithLabels("0", httpRequest.RequestUri.Host)
                     .Observe(activity.Duration.TotalSeconds);
             }
@@ -53,7 +56,7 @@ namespace Prometheus.Contrib.Diagnostics
 
         public override void OnException(Activity activity, object payload)
         {
-            PrometheusCounters.HttpClientRequestsErrors.Inc();
+            counters.HttpClientRequestsErrors.Inc();
         }
     }
 }
