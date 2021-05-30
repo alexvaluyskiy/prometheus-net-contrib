b0437dff10b9eec9a4f975c47b8cbe6e6fc6c006
Vladimir Belyakov <v.belyakov@smartcat.ai>
2021-05-30 10:48:47 +0300

    Fix SignalR metric name

--- src/prometheus-net.Contrib/EventListeners/Adapters/PrometheusSignalRCounterAdapter.cs
+++ src/prometheus-net.Contrib/EventListeners/Adapters/PrometheusSignalRCounterAdapter.cs
@@ -7,7 +7,7 @@ namespace Prometheus.Contrib.EventListeners.Adapters
         public const string EventSourceName = "Microsoft.AspNetCore.Http.Connections";
         
         internal readonly MeanCounter ConnectionsStarted = new MeanCounter("connections-started", "signalr_connections_started_total", "Total Connections Started");
-        internal readonly MeanCounter ConnectionsStopped = new MeanCounter("connections-stopped", "aspnetcore_requests_current_total", "Current Requests");
+        internal readonly MeanCounter ConnectionsStopped = new MeanCounter("connections-stopped", "signalr_connections_stopped_total", "Total Connections Stopped");
         internal readonly MeanCounter ConnectionsTimedOut = new MeanCounter("connections-timed-out", "signalr_connections_timed_out_total", "Total Connections Timed Out");
         internal readonly MeanCounter CurrentConnections = new MeanCounter("current-connections", "signalr_connections_current_total", "Current Connections");
         internal readonly MeanCounter ConnectionsDuration = new MeanCounter("connections-duration", "signalr_connections_duration_milliseconds", "Average Connection Duration");
